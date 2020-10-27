64276a64d0d7e6e786a4767520da7a41836ab825
Timothy Angus <timangus321@gmail.com>
2020-10-27 22:35:00 +0100

    add changes

---
+++ src/state-controlled-animation.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { TimelineLite, CSSPlugin } from "gsap";
+
+class StateControlledAnimation extends Component {
+
+    render(){
+    return (
+        <div>
+            <p>Hi I am a seperate component</p>
+        </div>
+    )
+    }
+
+};
+
+export default StateControlledAnimation;
